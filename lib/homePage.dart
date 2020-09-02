2b2f8c432a47715d349dd497f6c6d2c051a88483
Matheus <matheusvitor8143@gmail.com>
2020-09-02 17:02:26 -0300

    em producao

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -40,24 +40,64 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: ListView(
-        padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-        children: <Widget>[
-          Container(
-            height: (aux? (heightSize): 300),
-            color: Colors.red,
+      body: Container(
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+              height: (aux? (heightSize): 300),
+              color: Colors.red,
 
-          )
-        ],
-      )
+              child: MainView(),
+          ),
     );
   }
   Widget MainView(){
     return Column(
       children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints.expand(),
+                    child: Container(
+                      color: Colors.yellow,
+                      child: Text(
+                        'X 4',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                  flex: 2,
+                ),
+                Expanded(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints.expand(),
+                    child: Container(
+                      color: Colors.orangeAccent,
+                      child: Text(
+                        'O 4',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                  flex: 2,
+                ),
+              ],
+            ),
+            flex: 1,
+          ),
+        Expanded(
+          child: Container(
+            color: Colors.indigo,
+          ),
+          flex: 4,
+        ),
 
       ],
-    )
+    );
 
   }
 }
--- lib/settingsPage.dart
+++ lib/settingsPage.dart
@@ -1,18 +1,155 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SettingsPage extends StatelessWidget {
-  var x = 10;
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+
+  bool auxHeight = true;
+  String namePlay1 = 'Play1';
+  String namePlay2 = 'Play2';
+
+
   @override
   Widget build(BuildContext context) {
+    var heightSize = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'Settings',
         ),
-
       ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        children: <Widget>[
+          Container(
+            height: (auxHeight? (heightSize -90):100),
+            color: Colors.grey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: titleView(),
+                  flex: 1,
+                ),
 
+                Expanded(
+                  child: Container(
+                    color: Colors.grey,
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: textPlay1View(),
+                  ),
+                  flex: 1,
+                ),
+
+                Expanded(
+
+                  child: Container(
+                    color: Colors.grey,
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: textPlay2View(),
+                  ),
+                  flex: 1,
+                ),
+
+                Expanded(
+
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      width: 200,
+                      height: 80,
+                      child: buttonView(),
+                    ),
+                  ),
+                  flex: 4,
+                ),
+
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
+  Widget titleView(){
+    return Container(
+      child: Center(
+        child: Text(
+          'Iforme os dados',
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget textPlay1View(){
+    return TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'name play 1',
+        ),
+        onChanged: (text){
+          var aux  = text;
+          if(aux !=null){
+            setState(() {
+              namePlay1 = aux;
+            });
+          }
+        }
+    );
+  }
+
+  Widget textPlay2View(){
+    return TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'name play 2',
+        ),
+        onChanged: (text){
+          var aux  = text;
+          if(aux !=null){
+            setState(() {
+              namePlay2 = aux;
+            });
+          }
+        }
+    );
+  }
+
+  Widget buttonView(){
+    return FlatButton(
+
+      onPressed: () {
+
+      },
+      textColor: Colors.white,
+      color: Colors.blueAccent,
+      disabledColor: Colors.grey,
+      disabledTextColor: Colors.white,
+      highlightColor: Colors.orangeAccent,
+      child: Text(
+          'Send',
+          style: TextStyle(
+          fontSize: 20,
+      ),
+      ),
+    );
+  }
+
 }
+
+
+
+
+
+
